427216cb8b99a12c7fa4f9bf15e092c6107f65fc
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-10-21 22:43:44 -0300

    [ADD] invoice model added

--- invoices/models.py
+++ invoices/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Invoice(models.Model):
+    id = models.IntegerField(primary_key=True)
+    reference_month = models.IntegerField(null=False)
+    reference_year = models.IntegerField(null=False)
+    document = models.CharField(max_length=14, null=False, blank=False)
+    description = models.CharField(max_length=14, null=True, blank=True)
+    amount = models.DecimalField(
+        max_digits=16, decimal_places=2, null=False)
+    is_active = models.BooleanField(default=True, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deactive_at = models.DateTimeField(null=True)
