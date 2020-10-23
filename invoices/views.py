63593588546c03aa1ad1df45a8c0c90506ffaf59
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-10-22 23:52:08 -0300

    [UPDATE] creating initial  endpoints to perform http verbs

--- invoices/views.py
+++ invoices/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class InvoicesView(APIView):
+    """
+    Invoices API View
+    """
+
+    def get(self, request):
+        # retrieve items
+        return Response([])
+
+    def post(self, request, format=None):
+        # create
+        return Response(request.data, status=status.HTTP_201_CREATED)
+
+    def delete(self, request, pk, format=None):
+        # perform delete
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
