63139f16e5360c508b5075d161367475beb768a3
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-10-23 21:58:16 -0300

    [FIX] fixing description of invoices, on model layer

--- invoices/models.py
+++ invoices/models.py
@@ -6,7 +6,7 @@ class Invoice(models.Model):
     reference_month = models.IntegerField(null=False)
     reference_year = models.IntegerField(null=False)
     document = models.CharField(max_length=14, null=False, blank=False)
-    description = models.CharField(max_length=14, null=True, blank=True)
+    description = models.CharField(max_length=256, null=True, blank=True)
     amount = models.DecimalField(
         max_digits=16, decimal_places=2, null=False)
     is_active = models.BooleanField(default=True, null=False)
