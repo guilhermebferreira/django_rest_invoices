d84614d209cc35b9f2625bab1d5e58dcef038b6f
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-10-24 01:27:56 -0300

    [UPDATE] setup delete invoice endpoint api

--- invoices/views.py
+++ invoices/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from invoices.serializers import InvoiceSerializer
 from invoices.repository import InvoiceRepository
+from invoices.models import Invoice
 
 class InvoicesView(APIView):
     """
@@ -28,5 +29,16 @@ class InvoicesView(APIView):
 
     def delete(self, request, pk, format=None):
         # perform delete
+
+        try:
+            Invoice.objects.get(id=pk)
+        except Invoice.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        if pk:
+            resp, ok = self.repository.deactivate(int(pk))
+            if ok:
+                return Response(resp, status=status.HTTP_204_NO_CONTENT)
+            return Response(str(resp), status=status.HTTP_400_BAD_REQUEST)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
