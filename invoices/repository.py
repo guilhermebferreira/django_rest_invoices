0d0ad577e70ff50343fe2f2e40cd028cdc330fa0
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-10-24 01:26:10 -0300

    [UPDATE] creating method to deactiva invoices

--- invoices/repository.py
+++ invoices/repository.py
@@ -1,4 +1,4 @@
-from django.db import connection, transaction
+from django.db import connection
 
 from .models import Invoice
 
@@ -20,8 +20,8 @@ class InvoiceRepository:
                     "(reference_month, reference_year, document, description, amount, is_active) " \
                     "VALUES  " \
                     "({}, {}, '{}', '{}', {}, {})".format(data['reference_month'], data['reference_year'],
-                                                      data['document'], data['description'], data['amount'],
-                                                      data['is_active'])
+                                                          data['document'], data['description'], data['amount'],
+                                                          data['is_active'])
             with connection.cursor() as cursor:
                 cursor.execute(query)
                 data['id'] = cursor.lastrowid
@@ -30,8 +30,21 @@ class InvoiceRepository:
             print(str(ex))
             return ex, False
 
-    def update(self):
-        pass
+    def deactivate(self, pk):
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "UPDATE invoices_invoice SET is_active = FALSE, deactive_at=NOW() WHERE id={}".format(pk))
+                # row = Invoice.objects.raw("SELECT * FROM invoices_invoice WHERE id={}".format(pk))
+
+                cursor.execute("SELECT * FROM invoices_invoice WHERE id={}".format(pk))
+                row = self.__dictfetchone(cursor)
+
+            return row, True
+
+        except Exception as ex:
+            print(str(ex))
+            return ex, False
 
     def delete(self):
         pass
@@ -40,3 +53,17 @@ class InvoiceRepository:
         list = Invoice.objects.raw('SELECT * FROM invoice')
 
         return list
+
+    def __dictfetchall(self, cursor):
+        "Returns all rows from a cursor as a dict"
+        desc = cursor.description
+        return [
+            dict(zip([col[0] for col in desc], row))
+            for row in cursor.fetchall()
+        ]
+
+    def __dictfetchone(self, cursor):
+        "Returns all rows from a cursor as a dict"
+        desc = cursor.description
+        row = cursor.fetchone()
+        return  dict(zip([col[0] for col in desc], row))
