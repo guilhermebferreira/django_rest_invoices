7ae3dec6d6518ab0232f9b6eadfcdb936887d4bc
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-10-24 00:28:24 -0300

    [UPDATE] removing fields that will not be used on input validation

--- invoices/serializers.py
+++ invoices/serializers.py
@@ -4,7 +4,6 @@ from .models import Invoice
 
 
 class InvoiceSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
     reference_month = serializers.IntegerField(min_value=1, max_value=12, required=True)
     reference_year = serializers.IntegerField(min_value=1900, max_value=2999, required=True)
     document = serializers.CharField(max_length=14, required=True)
@@ -12,8 +11,6 @@ class InvoiceSerializer(serializers.ModelSerializer):
     amount = serializers.DecimalField(
         max_digits=16, decimal_places=2, required=True)
     is_active = serializers.BooleanField(default=True)
-    created_at = serializers.DateTimeField(read_only=True)
-    deactive_at = serializers.DateTimeField(required=False)
 
     class Meta:
         model = Invoice
