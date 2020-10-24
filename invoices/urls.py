6f9e16b9281a4cc78e4c4a0fca7ba8bd90bae958
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-10-24 01:25:36 -0300

    [UPDATE] adding url to receive requests with invoice pk

--- invoices/urls.py
+++ invoices/urls.py
@@ -5,5 +5,7 @@ from .views import InvoicesView
 app_name = "articles"
 
 urlpatterns = [
+    path('invoices', InvoicesView.as_view()),
     path('invoices/', InvoicesView.as_view()),
+    path('invoices/<int:pk>', InvoicesView.as_view()),
 ]
