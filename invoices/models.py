c316bf11141691f5611b6328a688a7ee4ca377c5
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-10-24 00:24:47 -0300

    [FIX] correcting column types

--- invoices/models.py
+++ invoices/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Invoice(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     reference_month = models.IntegerField(null=False)
     reference_year = models.IntegerField(null=False)
     document = models.CharField(max_length=14, null=False, blank=False)
@@ -10,5 +11,5 @@ class Invoice(models.Model):
     amount = models.DecimalField(
         max_digits=16, decimal_places=2, null=False)
     is_active = models.BooleanField(default=True, null=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(editable=False)
     deactive_at = models.DateTimeField(null=True)
